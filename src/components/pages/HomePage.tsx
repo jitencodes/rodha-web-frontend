05b0e9c35a6c2aa9f1e2b0f6aac5db9233485178
jitencodes <jitencodes@users.noreply.github.com>
2026-08-12 12:42:31 +0530

    fixed hero gradient

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -29,7 +29,7 @@ export function HomePage() {
 
     <HomePageBackground>
       {/* <DynamicBackground/> */}
-      <HomePageBodyTheme />
+      {/*<HomePageBodyTheme />*/}
 
       {/* <HomePageGradientAnchors /> */}
 
